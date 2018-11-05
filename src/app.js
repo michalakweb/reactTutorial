bb968d3cb7c1e5bf2b13fde1a825c01265197d56
Mateusz Michalak <michalakweb@gmail.com>
2018-11-05 14:03:58 +0700

    named exports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("It's running. Or is it?");
+// import './utils.js';
+import {square, add} from './utils.js';
+import { isAdult, canDrink } from './person.js';
+
+console.log("It's running");
+
+console.log(isAdult(10));
+console.log(canDrink(10)); 
---
+++ src/person.js
@@ -0,0 +1,2 @@
+export const isAdult = (x) => x >= 18 ? true : false;
+export const canDrink = (x) => x >= 21 ? true : false; 
